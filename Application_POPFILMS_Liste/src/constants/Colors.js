09bd017444869bc44555400c8e7a4b56731339cf
nafeaanas <nafeaanass15@gmail.com>
2023-03-07 23:46:16 +0100

    update UI for screens

--- Application_POPFILMS_Liste/src/constants/Colors.js
+++ Application_POPFILMS_Liste/src/constants/Colors.js
@@ -1,3 +1,5 @@
 export const Colors = {
-    BASIC_BACKGROUND: "#1A0938"
+    BASIC_BACKGROUND: "#1A0938",
+    ACTVE:"#ED1BA3",
+    BASIC_COLR:"#FFFFFF"
 };
--- Application_POPFILMS_Liste/src/screens/HomeScreen.jsx
+++ Application_POPFILMS_Liste/src/screens/HomeScreen.jsx
@@ -1,13 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, ScrollView} from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
 import { Colors } from '../constants/Colors';
+
+const Genres = ["All", "Action", "Comedy", "Romance", "Horror", "Sci-Fi"];
 
 const HomeScreen = () => {
   return (
     <ScrollView contentContainerStyle={styles.container}>
-      <StatusBar style="auto" />
-      <Text style={{color: 'white'}}>HomeScreen</Text>
+      <StatusBar 
+      style="auto" 
+      translucent={false}
+      backgroundColor={Colors.BASIC_BACKGROUND}
+      />
+      <View style={styles.headerContainer}>
+        <Text style={styles.headerTitle}>New Playing</Text>
+        <Text style={styles.headerSubTitle}>VIW ALL</Text>
+      </View>
+      <View>
+        <FlatList
+          data={Genres} 
+          horizontal
+          keyExtractor={(item) => item}
+          renderItem={({ item, index }) => {
+          return <View></View>;
+          }}
+        />
+      </View>
     </ScrollView>
   );
 }
@@ -16,9 +36,22 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: Colors.BASIC_BACKGROUND,
-    alignItems: 'center',
-    justifyContent:'center'
   },
+  headerContainer:{
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal:20,
+    paddingVertical: 10,
+  },
+  headerTitle:{
+    fontSize: 28,
+    color:Colors.BASIC_COLR
+  },
+  headerSubTitle:{
+    fontSize: 13,
+    color: Colors.ACTVE
+  }
 });
 
 export default HomeScreen;
